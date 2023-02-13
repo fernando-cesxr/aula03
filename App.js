630010091a57fc9412436151c2e7d9805cf834b8
Vinicius Caique <viniciuscaiquesilva@gmail.com>
2023-02-13 09:26:28 -0300

    text-input e estilos

--- App.js
+++ App.js
@@ -1,13 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import { StyleSheet, Text, TextInput, View, Platform, Button, Alert } from 'react-native';
+import React from 'react'
+
+export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      mensagem: ''
+    }
+  }
+
+  mudouMensagem = msg => {
+    this.setState({ mensagem: msg })
+  }
+
+
+  render() {
+
+    const { mensagem } = this.state
+
+    return (
+      <View style={styles.container}>
+        {Platform.OS === 'android' ? (
+          <Text style={styles.text}>Android</Text>
+        ) : (
+          <Text style={styles.text}>IOS</Text>
+        )}
+        <Text style={styles.text}>FIAP</Text>
+        {/* <Text style={[styles.text, { color: 'gray' }]}>Portal do Aluno</Text> */}
+        <Text style={[styles.text, styles.textColor]}>Portal do Aluno</Text>
+        <TextInput
+          style={styles.textInput}
+          placeholder='Username'
+          onChangeText={this.mudouMensagem}
+        />
+        <TextInput
+          style={styles.textInput}
+          placeholder='Password'
+        />
+        <Button 
+          title='Clique'
+          onPress={() => alert( mensagem )}
+        />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +56,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontSize: 25,
+  },
+  textColor: {
+    color: 'gray',
+  },
+  textInput: {
+    borderWidth: 2,
+    borderColor: 'gray',
+    borderRadius: 10,
+    padding: 20,
+    margin: 10,
+    width: 300,
+  }
 });
